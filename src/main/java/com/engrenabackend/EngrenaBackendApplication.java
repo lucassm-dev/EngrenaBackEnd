787bfe2cde49d97063a0fb55fa72a8a350be319c
Lucas Mendes <lucassabadinimendes@gmail.com>
2026-03-11 19:03:04 -0300

    Primeiro Commit

---
+++ src/main/java/com/engrenabackend/EngrenaBackendApplication.java
@@ -0,0 +1,12 @@
+package com.engrenabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EngrenaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EngrenaBackendApplication.class, args);
+    }
+}
