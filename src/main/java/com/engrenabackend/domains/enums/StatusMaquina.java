1990e38dcc58598b5d74dba6943632719f0c44e7
Lucas Mendes <lucassabadinimendes@gmail.com>
2026-03-13 23:17:24 -0300

    feat: adiciona enum status maquina

---
+++ src/main/java/com/engrenabackend/domains/enums/StatusMaquina.java
@@ -0,0 +1,37 @@
+package com.engrenabackend.domains.enums;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public enum StatusMaquina {
+    OPERANDO(0, "OPERANDO"), EM_MANUTENCAO(1, "EM_MANUTENCAO"), PARADA(2, "PARADA"), DESATIVADA(3, "DESATIVADA");
+
+    private Integer id;
+    private String descricao;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public static StatusMaquina toEnum(Integer id){
+        if(id == null) return null;
+        for(StatusMaquina statusMaquina : StatusMaquina.values()){
+            if(id.equals(statusMaquina.getId())){
+                return statusMaquina;
+            }
+        }
+        throw new IllegalArgumentException("Status da Máquina Inválida!");
+    }
+}
