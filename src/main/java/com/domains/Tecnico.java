f4da2b41107f35ea5fb766fad6766bc62705ad18
Lucas Mendes <lucassabadinimendes@gmail.com>
2026-03-15 14:04:20 -0300

    fix: altera entidade tecnico

---
+++ src/main/java/com/domains/Tecnico.java
@@ -0,0 +1,51 @@
+package com.domains;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "tb_tecnico")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@SequenceGenerator(
+        name = "tb_tecnico",
+        sequenceName = "tb_tecnico",
+        allocationSize = 1
+)
+public class Tecnico {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_tecnico")
+    private Long id;
+
+    @NotBlank
+    @Column(nullable=false, length=70)
+    private String nome;
+
+
+    @NotBlank
+    @Column(nullable = false, length = 30, unique = true)
+    private String cpf;
+
+    @NotBlank
+    @Size(min = 10, max = 15)
+    @Column(nullable = false, length = 15)
+    private String telefone;
+
+    @NotBlank
+    @Column(nullable = false, length = 30, unique = true)
+    private String email;
+
+    @OneToMany(mappedBy = "tecnico", fetch = FetchType.LAZY)
+    private List<Manutencao> manutencoes = new ArrayList<>();
+}
+
+
